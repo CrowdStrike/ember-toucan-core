d736e0b5148b150a64dda55dc2efb8695968d3cb
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-01-19 12:50:24 -0500

    feat: Add Button component

---
+++ docs/components/button/index.md
@@ -0,0 +1,148 @@
+# Button
+
+Buttons are clickable elements used primarily for actions. Button content expresses what action will occur when the user interacts with it.
+
+## Variants
+
+You can customize the appearance of the button with the `@variant` component argument.
+
+<div class="flex gap-x-4">
+  <Button @variant="primary">Primary</Button>
+  <Button @variant="secondary">Secondary</Button>
+  <Button @variant="destructive">Destructive</Button>
+  <Button @variant="link">Link</Button>
+  <Button @variant="quiet">Quiet</Button>
+  <Button @variant="bare">Bare</Button>
+</div>
+
+## Type
+
+To provide the [type attribute](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#attr-type) to the Button, the `@type` component argument is used.
+
+```hbs
+<Button @type='submit'>Submit</Button>
+```
+
+## Handling Clicks
+
+To handle click events use the `@onClick` component argument.
+
+```hbs
+<Button @onClick={{this.handleClick}}>Click Me</Button>
+```
+
+## Disabled State
+
+`aria-disabled` is used over the `disabled` attribute so that screenreaders can still focus the element. To set the button as disabled, use `@isDisabled`.
+
+```hbs
+<Button @isDisabled={{true}}>Disabled</Button>
+```
+
+A disabled named block is provided so that users can optionally render additional content when the button is disabled.
+
+```hbs
+<Button @isDisabled={{true}}>
+  <:disabled>
+    <svg
+      class='h-4 w-4'
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+    >
+      <path
+        d='M18.644 21h-13.2a.945.945 0 01-1-1v-7.2a.945.945 0 011-1h13.1a.945.945 0 011 1V20a.808.808 0 01-.225.725.966.966 0 01-.675.275zm-10.9-9.2V7.3a4.3 4.3 0 118.6 0v4.5m-4.3 3.7v2'
+        fill='none'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+      />
+    </svg>
+  </:disabled>
+
+  <:default>
+    Disabled
+  </:default>
+</Button>
+```
+
+<div class="flex gap-x-4">
+{{#each (array "primary" "secondary" "destructive" "link" "quiet" "bare") as |variant|}}
+<Button @variant={{variant}} @isDisabled={{true}}>
+<:disabled>
+<svg
+      class='h-4 w-4'
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+    >
+<path
+        d="M18.644 21h-13.2a.945.945 0 01-1-1v-7.2a.945.945 0 011-1h13.1a.945.945 0 011 1V20a.808.808 0 01-.225.725.966.966 0 01-.675.275zm-10.9-9.2V7.3a4.3 4.3 0 118.6 0v4.5m-4.3 3.7v2"
+        fill='none'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+      />
+</svg>
+</:disabled>
+
+<:default>
+{{variant}}
+</:default>
+</Button>
+{{/each}}
+
+</div>
+
+## Loading State
+
+Button exposes an `@isLoading` component argument. The button content will be only visible to screenreaders.
+
+```hbs
+<Button @isLoading={{true}}>Loading…</Button>
+```
+
+A loading named block is also provided for providing custom loading content.
+
+```hbs
+<Button @isLoading={{true}}>
+  <:loading>
+    <svg
+      class='h-4 w-4 animate-spin'
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      stroke='currentColor'
+      viewBox='0 0 24 24'
+    ><path
+        d='M5.95 5.7L7 6.75 8.05 7.8m8.4 8.4l.95.95.95.95m.2-12.4L17.5 6.75 16.45 7.8M6.35 12h-3.1m17.5 0h-2.6m-5.9 9v-3.1m0-14.9v3.1'
+        fill='none'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='2'
+      /></svg>
+  </:loading>
+  <:default>
+    Loading…
+  </:default>
+</Button>
+```
+
+<div class="flex gap-x-4">
+{{#each (array "primary" "secondary" "destructive" "link" "quiet" "bare") as |variant|}}
+<Button @variant={{variant}} @isLoading={{true}}>
+<:loading>
+<svg class='h-4 w-4 animate-spin' xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" viewBox="0 0 24 24"><path d="M5.95 5.7L7 6.75 8.05 7.8m8.4 8.4l.95.95.95.95m.2-12.4L17.5 6.75 16.45 7.8M6.35 12h-3.1m17.5 0h-2.6m-5.9 9v-3.1m0-14.9v3.1" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" /></svg>
+</:loading>
+
+<:default>
+{{variant}}
+</:default>
+</Button>
+{{/each}}
+
+</div>
---
+++ test-app/tests/integration/components/button-test.ts
@@ -0,0 +1,162 @@
+import { click, render, setupOnerror } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+
+import { setupRenderingTest } from 'test-app/tests/helpers';
+
+module('Integration | Component | button', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`
+      <Button>
+        text
+      </Button>
+    `);
+
+    assert.dom('button').hasText('text');
+    assert.dom('button').hasNoAttribute('aria-disabled');
+    assert
+      .dom('button')
+      .hasAttribute('type', 'button', 'Expected default type to be "button"');
+  });
+
+  test('it yields a loading named block when `@isLoading={{true}}', async function (assert) {
+    await render(hbs`
+      <Button @isLoading={{true}}>
+        <:loading>
+          <span data-test-selector="loading">loading state</span>
+        </:loading>
+      </Button>
+    `);
+
+    assert
+      .dom('[data-test-selector="loading"]')
+      .exists('Expect to have loading named block rendered');
+  });
+
+  test('it does not render the content in the loading named block when `@isLoading={{false}}', async function (assert) {
+    await render(hbs`
+      <Button @isLoading={{false}}>
+        <:loading>
+          <span data-test-selector="loading">should not be visible since isLoading is false</span>
+        </:loading>
+        <:default>
+          <span data-test-selector="default" />
+        </:default>
+      </Button>
+    `);
+
+    assert
+      .dom('[data-test-selector="loading"]')
+      .doesNotExist('Expected to NOT have loading named block rendered');
+
+    assert
+      .dom('[data-test-selector="default"]')
+      .exists('Expect to have default named block rendered');
+  });
+
+  test('it yields a disabled named block when `@isDisabled={{true}}', async function (assert) {
+    await render(hbs`
+      <Button @isDisabled={{true}}>
+        <:disabled>
+          <span data-test-selector="disabled">disabled state</span>
+        </:disabled>
+      </Button>
+    `);
+
+    assert
+      .dom('[data-test-selector="disabled"]')
+      .exists('Expect to have disabled named block rendered');
+  });
+
+  test('it does not render the content in the disabled named block when `@isDisabled={{false}}', async function (assert) {
+    await render(hbs`
+      <Button @isDisabled={{false}}>
+        <:disabled>
+          <span data-test-selector="disabled">should not be visible since isDisabled is false</span>
+        </:disabled>
+        <:default>
+          <span data-test-selector="default" />
+        </:default>
+      </Button>
+    `);
+
+    assert
+      .dom('[data-test-selector="disabled"]')
+      .doesNotExist('Expected to NOT have disabled named block rendered');
+
+    assert
+      .dom('[data-test-selector="default"]')
+      .exists('Expect to have default named block rendered');
+  });
+
+  test('it sets `aria-disabled="true"` when `@isDisabled={{true}}', async function (assert) {
+    await render(hbs`
+      <Button @isDisabled={{true}}>
+        disabled
+      </Button>
+    `);
+
+    assert.dom('button').hasAttribute('aria-disabled', 'true');
+  });
+
+  test('it sets `type` based on `@type', async function (assert) {
+    await render(hbs`
+      <Button @type="submit">
+        button
+      </Button>
+    `);
+
+    assert.dom('button').hasAttribute('type', 'submit');
+  });
+
+  test('it calls the provided `@onClick`', async function (assert) {
+    this.set('onClick', () => assert.step('clicked'));
+
+    await render(hbs`
+      <Button @onClick={{this.onClick}}>
+        button
+      </Button>
+    `);
+
+    assert.verifySteps([]);
+
+    await click('button');
+
+    assert.verifySteps(['clicked']);
+  });
+
+  test('it does NOT call the provided `@onClick` if `@isDisabled={{true}}', async function (assert) {
+    this.set('onClick', () => assert.step('clicked'));
+
+    await render(hbs`
+      <Button @isDisabled={{true}} @onClick={{this.onClick}}>
+        button
+      </Button>
+    `);
+
+    assert.verifySteps([]);
+
+    await click('button');
+
+    assert.verifySteps([]);
+  });
+
+  test('it throws an assertion error if provided with an unsupported `@variant`', async function (assert) {
+    assert.expect(1);
+
+    setupOnerror((e: Error) => {
+      assert.ok(
+        e.message.includes('Invalid variant for Button'),
+        'Expected assertion error message'
+      );
+    });
+
+    await render(hbs`
+      <Button @variant="not-a-real-variant">
+        button
+      </Button>
+    `);
+  });
+});
