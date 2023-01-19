90a9c938e7ffe7d2a40dbe7d28efddcc9ca00b20
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-19 11:01:24 -0500

    Fix hbs quote setting

--- test-app/.prettierrc.js
+++ test-app/.prettierrc.js
@@ -12,6 +12,12 @@ module.exports = {
         singleQuote: true,
       },
     },
+    {
+      files: '*.hbs',
+      options: {
+        singleQuote: false,
+      },
+    },
     // this was required to make the VSCode + Prettier work correctly with <template>, see https://github.com/gitKrystan/prettier-plugin-ember-template-tag/issues/38
     // we should roll this back once that issue has been fixed!
     {
