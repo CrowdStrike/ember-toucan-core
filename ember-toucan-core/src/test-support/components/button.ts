ad93eb556b7d02c5e3a7dba6550e98a18c036d2f
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-01-24 15:41:02 -0500

    feat: Expose test-support FPO

---
+++ ember-toucan-core/src/test-support/components/button.ts
@@ -0,0 +1,23 @@
+import { click } from '@ember/test-helpers';
+
+import { PageObject } from 'fractal-page-object';
+
+export class Button extends PageObject {
+  async click() {
+    if (this.element) {
+      await click(this.element);
+    }
+  }
+
+  get isLoading() {
+    return Boolean(this.element?.querySelector('[data-loading]'));
+  }
+
+  get isDisabled() {
+    return Boolean(this.element?.hasAttribute('aria-disabled'));
+  }
+
+  get text() {
+    return this.element?.textContent?.trim();
+  }
+}
--- test-app/tests/integration/components/button-test.ts
+++ test-app/tests/integration/components/button-test.ts
@@ -1,51 +1,59 @@
-import { click, render, setupOnerror } from '@ember/test-helpers';
+import { render, setupOnerror } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { module, test } from 'qunit';
 
 import { setupRenderingTest } from 'test-app/tests/helpers';
 
+import { Button } from '@crowdstrike/ember-toucan-core/test-support';
+
 module('Integration | Component | button', function (hooks) {
   setupRenderingTest(hooks);
 
+  let button = new Button('[data-button]');
+
   test('it renders', async function (assert) {
     await render(hbs`
-      <Button>
+      <Button data-button>
         text
       </Button>
     `);
 
-    assert.dom('button').hasText('text');
-    assert.dom('button').hasNoAttribute('aria-disabled');
+    assert.strictEqual(button.text, 'text');
+    assert.false(button.isDisabled);
     assert
-      .dom('button')
+      .dom(button.element)
       .hasAttribute('type', 'button', 'Expected default type to be "button"');
   });
 
   test('it yields a loading named block when `@isLoading={{true}}', async function (assert) {
     await render(hbs`
-      <Button @isLoading={{true}}>
+      <Button @isLoading={{true}} data-button>
         <:loading>
-          <span data-test-loading>loading state</span>
+          <span data-test-loading-content>loading state</span>
         </:loading>
       </Button>
     `);
 
+    assert.true(button.isLoading);
+
     assert
-      .dom('[data-test-loading]')
+      .dom('[data-test-loading-content]')
       .exists('Expect to have loading named block rendered');
   });
 
   test('it does not render the content in the loading named block when `@isLoading={{false}}', async function (assert) {
     await render(hbs`
-      <Button @isLoading={{false}}>
+      <Button @isLoading={{false}} data-button>
         <:loading>
-          <span data-test-loading>should not be visible since isLoading is false</span>
+          <span data-test-loading-content>should not be visible since isLoading is false</span>
         </:loading>
         <:default>
           <span data-test-default />
         </:default>
       </Button>
     `);
+
+    assert.false(button.isLoading);
 
     assert
       .dom('[data-test-loading]')
@@ -56,25 +64,35 @@ module('Integration | Component | button', function (hooks) {
       .exists('Expect to have default named block rendered');
   });
 
+  test('it sets `aria-disabled="true"` when `@isDisabled={{true}}', async function (assert) {
+    await render(hbs`
+      <Button @isDisabled={{true}} data-button>
+        disabled
+      </Button>
+    `);
+
+    assert.true(button.isDisabled);
+  });
+
   test('it yields a disabled named block when `@isDisabled={{true}}', async function (assert) {
     await render(hbs`
-      <Button @isDisabled={{true}}>
+      <Button @isDisabled={{true}} data-button>
         <:disabled>
-          <span data-test-disabled>disabled state</span>
+          <span data-test-disabled-content>disabled state</span>
         </:disabled>
       </Button>
     `);
 
     assert
-      .dom('[data-test-disabled]')
+      .dom('[data-test-disabled-content]')
       .exists('Expect to have disabled named block rendered');
   });
 
   test('it does not render the content in the disabled named block when `@isDisabled={{false}}', async function (assert) {
     await render(hbs`
-      <Button @isDisabled={{false}}>
+      <Button @isDisabled={{false}} data-button>
         <:disabled>
-          <span data-test-disabled>should not be visible since isDisabled is false</span>
+          <span data-test-disabled-content>should not be visible since isDisabled is false</span>
         </:disabled>
         <:default>
           <span data-test-default />
@@ -83,7 +101,7 @@ module('Integration | Component | button', function (hooks) {
     `);
 
     assert
-      .dom('[data-test-disabled]')
+      .dom('[data-test-disabled-content]')
       .doesNotExist('Expected to NOT have disabled named block rendered');
 
     assert
@@ -91,28 +109,18 @@ module('Integration | Component | button', function (hooks) {
       .exists('Expect to have default named block rendered');
   });
 
-  test('it sets `aria-disabled="true"` when `@isDisabled={{true}}', async function (assert) {
-    await render(hbs`
-      <Button @isDisabled={{true}}>
-        disabled
-      </Button>
-    `);
-
-    assert.dom('button').hasAttribute('aria-disabled', 'true');
-  });
-
   test('it calls the provided `@onClick`', async function (assert) {
     this.set('onClick', () => assert.step('clicked'));
 
     await render(hbs`
-      <Button @onClick={{this.onClick}}>
+      <Button @onClick={{this.onClick}} data-button>
         button
       </Button>
     `);
 
     assert.verifySteps([]);
 
-    await click('button');
+    await button.click();
 
     assert.verifySteps(['clicked']);
   });
@@ -121,14 +129,14 @@ module('Integration | Component | button', function (hooks) {
     this.set('onClick', () => assert.step('clicked'));
 
     await render(hbs`
-      <Button @isDisabled={{true}} @onClick={{this.onClick}}>
+      <Button @isDisabled={{true}} @onClick={{this.onClick}} data-button>
         button
       </Button>
     `);
 
     assert.verifySteps([]);
 
-    await click('button');
+    await button.click();
 
     assert.verifySteps([]);
   });
