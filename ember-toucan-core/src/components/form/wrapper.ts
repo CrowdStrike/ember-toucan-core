fcd1cb12d855819e5321e28bd5a26a3ecaddff2a
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-02-13 14:08:41 -0500

    Use a union for the Wrapper type

--- ember-toucan-core/src/components/form/wrapper.ts
+++ ember-toucan-core/src/components/form/wrapper.ts
@@ -8,24 +8,23 @@ import Label from './label';
 interface ToucanFormWrapperComponentSignature {
   Element: HTMLDivElement;
   Args: {};
-  Blocks: {
-    default?: [
-      {
-        Label: typeof Label;
-        Hint: typeof Hint;
-        Control: typeof Control;
-        Error: typeof Error;
+  Blocks:
+    | {
+        default: [
+          {
+            Label: typeof Label;
+            Hint: typeof Hint;
+            Control: typeof Control;
+            Error: typeof Error;
+          }
+        ];
       }
-    ];
-    hint?: any;
-    control?: any;
-    // (property) ToucanFormWrapperComponent.Hint: TemplateOnlyComponent<ToucanFormHintComponentSignature>
-    // Argument of type '[TemplateOnlyComponent<ToucanFormHintComponentSignature>]' is not assignable to parameter of type 'TemplateOnlyComponent<ToucanFormLabelComponentSignature>'.
-    // Property '[Data]' is missing in type '[TemplateOnlyComponent<ToucanFormHintComponentSignature>]' but required in type 'TemplateOnlyComponent<ToucanFormLabelComponentSignature>'.glint:ts(2345)
-    // label?: typeof Label;
-    label?: any;
-    error?: any;
-  };
+    | {
+        hint: typeof Hint;
+        control: typeof Control;
+        label: typeof Label;
+        error: typeof Error;
+      };
 }
 
 export default class ToucanFormWrapperComponent extends Component<ToucanFormWrapperComponentSignature> {
