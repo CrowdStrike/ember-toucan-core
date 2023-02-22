d7f783de16fb3db640ac8e52340c46f69a130d36
Nicole Chung <771170+nicolechung@users.noreply.github.com>
2023-02-22 11:26:03 -0500

    feat: InputField, add @value and @onChange

--- ember-toucan-core/src/components/form/input-field.ts
+++ ember-toucan-core/src/components/form/input-field.ts
@@ -1,14 +1,17 @@
 import Component from '@glimmer/component';
 import { assert } from '@ember/debug';
+import { action } from '@ember/object';
 
 interface ToucanFormInputFieldComponentSignature {
   Element: HTMLInputElement;
   Args: {
+    error?: string;
     label: string;
     hint?: string;
-    error?: string;
     isDisabled?: boolean;
+    onChange?: (value: string, e: Event | InputEvent) => void;
     readonly?: boolean;
+    value?: string;
   };
   Blocks: {
     default: [];
@@ -22,5 +25,14 @@ export default class ToucanFormInputFieldComponent extends Component<ToucanFormI
   ) {
     assert('input field requires a label', args.label !== undefined);
     super(owner, args);
+  }
+
+  @action
+  handleInput(e: Event | InputEvent): void {
+    assert('Expected HTMLInputElement', e.target instanceof HTMLInputElement);
+
+    if (this.args.onChange) {
+      this.args.onChange(e.target.value, e);
+    }
   }
 }
