9aade1576cd457a74fa6ea994c222e560a0924da
Nicole Chung <771170+nicolechung@users.noreply.github.com>
2023-02-22 15:49:55 -0500

    feat: Form::Controls::Input component
    - update rollup config
    
    - added Form::Controls::Input
    
    - added new component to the template registry
    
    - added new component to the unpublished development types

--- ember-toucan-core/src/components/form/controls/input.ts
+++ ember-toucan-core/src/components/form/controls/input.ts
@@ -2,32 +2,17 @@ import Component from '@glimmer/component';
 import { assert } from '@ember/debug';
 import { action } from '@ember/object';
 
-interface ToucanFormInputFieldComponentSignature {
+interface ToucanFormControlInputComponentSignature {
   Element: HTMLInputElement;
   Args: {
-    error?: string;
-    label: string;
-    hint?: string;
     isDisabled?: boolean;
     onChange?: (value: string, e: Event | InputEvent) => void;
     readonly?: boolean;
-    rootTestSelector?: string;
     value?: string;
-  };
-  Blocks: {
-    default: [];
   };
 }
 
-export default class ToucanFormInputFieldComponent extends Component<ToucanFormInputFieldComponentSignature> {
-  constructor(
-    owner: unknown,
-    args: ToucanFormInputFieldComponentSignature['Args']
-  ) {
-    assert('input field requires a label', args.label !== undefined);
-    super(owner, args);
-  }
-
+export default class ToucanFormControlInputComponent extends Component<ToucanFormControlInputComponentSignature> {
   @action
   handleInput(e: Event | InputEvent): void {
     assert('Expected HTMLInputElement', e.target instanceof HTMLInputElement);
--- ember-toucan-core/src/template-registry.ts
+++ ember-toucan-core/src/template-registry.ts
@@ -1,12 +1,12 @@
 import type ButtonComponent from './components/button';
 import type CheckboxFieldComponent from './components/form/checkbox-field';
-import type CheckboxConrolComponent from './components/form/controls/checkbox';
-import type TextareaControlComponent from './components/form/controls/textarea';
+import type CheckboxControlComponent from './components/form/controls/checkbox';
 import type InputControlComponent from './components/form/controls/input';
+import type TextareaControlComponent from './components/form/controls/textarea';
 import type FieldComponent from './components/form/field';
 import type FieldsetComponent from './components/form/fieldset';
-import type TextareaFieldComponent from './components/form/textarea-field';
 import type InputField from './components/form/input-field';
+import type TextareaFieldComponent from './components/form/textarea-field';
 
 export default interface Registry {
   Button: typeof ButtonComponent;
@@ -14,8 +14,8 @@ export default interface Registry {
   'Form::Fieldset': typeof FieldsetComponent;
   'Form::CheckboxField': typeof CheckboxFieldComponent;
   'Form::InputField': typeof InputField;
-  'Form::Controls::Input' typeof InputControlComponent;
-  'Form::TextareaField': typeof TextareaFieldComponent;
-  'Form::Controls::Checkbox': typeof CheckboxConrolComponent;
+  'Form::Controls::Checkbox': typeof CheckboxControlComponent;
+  'Form::Controls::Input': typeof InputControlComponent;
   'Form::Controls::Textarea': typeof TextareaControlComponent;
+  'Form::TextareaField': typeof TextareaFieldComponent;
 }
