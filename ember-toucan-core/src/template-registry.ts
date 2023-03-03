4bcda8664fb00175ec53dee956ef4c7f495c6292
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-03-03 09:57:59 -0500

    feat: Add Fieldset component

--- ember-toucan-core/src/template-registry.ts
+++ ember-toucan-core/src/template-registry.ts
@@ -3,11 +3,13 @@ import type CheckboxFieldComponent from './components/form/checkbox-field';
 import type CheckboxConrolComponent from './components/form/controls/checkbox';
 import type TextareaControlComponent from './components/form/controls/textarea';
 import type FieldComponent from './components/form/field';
+import type FieldsetComponent from './components/form/fieldset';
 import type TextareaFieldComponent from './components/form/textarea-field';
 
 export default interface Registry {
   Button: typeof ButtonComponent;
   'Form::Field': typeof FieldComponent;
+  'Form::Fieldset': typeof FieldsetComponent;
   'Form::CheckboxField': typeof CheckboxFieldComponent;
   'Form::TextareaField': typeof TextareaFieldComponent;
   'Form::Controls::Checkbox': typeof CheckboxConrolComponent;
