7a5ba078ad35fb16d859b474388a3e9ed1882058
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-03-06 14:52:07 -0500

    design-testing: Test out new error-focus styles

--- docs-app/tailwind.config.js
+++ docs-app/tailwind.config.js
@@ -5,6 +5,17 @@ const { tailwindConfig } = require('@crowdstrike/ember-oss-docs/tailwind');
 
 const config = tailwindConfig(__dirname, {
   content: [path.join(__dirname, '../docs/**/*.md')],
+
+  // Temporary for testing for a preview URL.
+  // DO NOT MERGE!
+  theme: {
+    extend: {
+      boxShadow: {
+        'form-error-focus':
+          'inset 0 0 0 2px var(--critical), 0 0 0 1px var(--surface-base), 0 0 0 3px var(--focus)',
+      },
+    },
+  },
 });
 
 module.exports = config;
