ca7687cbbacafd33c016ae751eac20d6e6c36a10
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-03-31 11:22:07 -0400

    fix: Resolve error from lint

--- docs-app/.template-lintrc.js
+++ docs-app/.template-lintrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   extends: 'recommended',
+
+  overrides: [
+    {
+      files: ['**/*.gjs', '**/*.gts'],
+      rules: {
+        'no-implicit-this': { allow: ['DARK'] },
+      },
+    },
+  ],
 };
