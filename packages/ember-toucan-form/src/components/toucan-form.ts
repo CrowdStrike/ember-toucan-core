0dd13537ea66984b0409db689303773ee6f8ef79
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-04-11 15:35:11 -0400

    fix: Use headless-form field type

--- packages/ember-toucan-form/src/components/toucan-form.ts
+++ packages/ember-toucan-form/src/components/toucan-form.ts
@@ -23,9 +23,10 @@ export interface ToucanFormComponentSignature<
     default: [
       {
         Textarea?: WithBoundArgs<typeof TextareaFieldComponent, 'form'>;
-        // TODO: Need to figure out a type for this
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Field: any;
+        Field: HeadlessFormComponentSignature<
+          DATA,
+          SUBMISSION_VALUE
+        >['Blocks']['default'][0]['Field'];
       }
     ];
   };
