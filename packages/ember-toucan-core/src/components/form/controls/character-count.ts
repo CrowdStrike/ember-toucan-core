e02c074d07cb56a315b679410016c5f29530f251
Nicole Chung <771170+nicolechung@users.noreply.github.com>
2023-04-14 20:41:12 -0400

    feat: Make CharacterCount a dumb component

--- packages/ember-toucan-core/src/components/form/controls/character-count.ts
+++ packages/ember-toucan-core/src/components/form/controls/character-count.ts
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { assert } from '@ember/debug';
 
 export interface ToucanFormControlCharacterCountComponentSignature {
   Element: HTMLElement;
@@ -13,36 +12,8 @@ export interface ToucanFormControlCharacterCountComponentSignature {
      * The max amount allowed for this input
      */
     max?: number;
-    id: string;
   };
 }
 
-export default class ToucanFormControlCharacterCount extends Component<ToucanFormControlCharacterCountComponentSignature> {
-  constructor(
-    owner: unknown,
-    args: ToucanFormControlCharacterCountComponentSignature['Args']
-  ) {
-    assert(
-      'An "@id" argument is required, the id should be from the related input',
-      args.id
-    );
-    super(owner, args);
-  }
-
-  get initialCount() {
-    const id = this.args.id;
-    const input = <HTMLInputElement>document.getElementById(id);
-
-    if (input) {
-      return input.value.length;
-    }
-
-    return 0;
-  }
-
-  get currentCount() {
-    if (!this.args.current) return this.initialCount;
-
-    return this.args.current;
-  }
-}
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
+export default class ToucanFormControlCharacterCount extends Component<ToucanFormControlCharacterCountComponentSignature> {}
