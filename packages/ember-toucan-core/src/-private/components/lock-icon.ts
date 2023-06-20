8fab184136fd13abc151431d00c709ba4596fd0f
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-06-20 15:30:30 -0400

    Add lock icon component

---
+++ packages/ember-toucan-core/src/-private/components/lock-icon.ts
@@ -0,0 +1,11 @@
+import templateOnlyComponent from '@ember/component/template-only';
+
+export interface ToucanLockIconComponentSignature {
+  Element: Element;
+  Args: {};
+  Blocks: {
+    default: [];
+  };
+}
+
+export default templateOnlyComponent<ToucanLockIconComponentSignature>();
