6403ccbe94f68f28334de59bab943de8f77383a5
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-06-29 10:38:20 -0400

    toucan-form: Update JSdoc for submit+reset

--- packages/ember-toucan-form/src/components/toucan-form.ts
+++ packages/ember-toucan-form/src/components/toucan-form.ts
@@ -42,12 +42,16 @@ export interface ToucanFormComponentSignature<
         /**
          * Yielded action that will trigger form validation and submission, same as when triggering the native `submit` event on the form.
          *
+         * View ember-headless-form's documentation for more information.
+         *
          * Note that calling this directly is **not** required for most cases. The implementation only requires a button tag with the `type="submit"` attribute set; however, this is exposed for more complex cases.
          */
         submit: () => void;
 
         /**
          * Yielded action that will reset form state, same as when triggering the native `reset` event on the form.
+         *
+         * View ember-headless-form's documentation for more information.
          */
         reset: () => void;
       }
