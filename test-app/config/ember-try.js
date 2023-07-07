6496fd457e654c1bb98d8ae52d4fb2c44533035e
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-07-07 09:04:12 -0400

    fix: Add embroider/compat as an explicit dep

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -44,24 +44,27 @@ module.exports = async function () {
       embroiderOptimized({
         name: 'ember-lts-4.8 + embroider-optimized',
         npm: {
+          dependencies: {
+            '@embroider/compat': '3.1.4',
+          },
           devDependencies: {
             'ember-source': '~4.8.0',
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
+            '@embroider/core': '3.1.2',
+
+            '@embroider/webpack': '3.1.2',
           },
         },
       }),
       embroiderOptimized({
         name: 'ember-release + embroider-optimized',
         npm: {
+          dependencies: {
+            '@embroider/compat': '3.1.4',
+          },
           devDependencies: {
             'ember-source': releaseVersion,
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
+            '@embroider/core': '3.1.2',
+            '@embroider/webpack': '3.1.2',
           },
         },
       }),
