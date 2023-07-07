0a1ec6504e9e0f035a68874609a158465e7f8ab1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-07 19:42:48 -0400

    Only test embroider strict

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   let releaseVersion = await getChannelURL('release');
@@ -41,33 +40,6 @@ module.exports = async function () {
           },
         },
       },
-      embroiderOptimized({
-        name: 'ember-lts-4.8 + embroider-optimized',
-        npm: {
-          dependencies: {
-            '@embroider/compat': '3.1.4',
-          },
-          devDependencies: {
-            'ember-source': '~4.8.0',
-            '@embroider/core': '3.1.2',
-
-            '@embroider/webpack': '3.1.2',
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-release + embroider-optimized',
-        npm: {
-          dependencies: {
-            '@embroider/compat': '3.1.4',
-          },
-          devDependencies: {
-            'ember-source': releaseVersion,
-            '@embroider/core': '3.1.2',
-            '@embroider/webpack': '3.1.2',
-          },
-        },
-      }),
     ],
   };
 };
