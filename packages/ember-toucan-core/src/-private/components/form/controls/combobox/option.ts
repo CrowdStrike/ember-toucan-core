bb353bbc2bb4a86ef95f09ac6e4e399ea910c730
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-07-07 14:53:57 -0400

    toucan-core: Add JSDoc for option

--- packages/ember-toucan-core/src/-private/components/form/controls/combobox/option.ts
+++ packages/ember-toucan-core/src/-private/components/form/controls/combobox/option.ts
@@ -5,14 +5,51 @@ import Check from '../../../../../-private/icons/check';
 
 interface ToucanFormComboboxOptionControlComponentSignature {
   Args: {
+    /**
+     * When true, means that the option is currently hovered over with a mouse
+     * or "focused" with a keyboard.
+     */
     isActive?: boolean;
+
+    /**
+     * Sets the underlying, hidden input element to disabled.
+     */
     isDisabled?: boolean;
+
+    /**
+     * Sets the underlying, hidden input element to readonly.
+     */
     isReadOnly?: boolean;
+
+    /**
+     * When set to true, the list item will have `aria-selected` set to true
+     * and have selected styling.
+     */
     isSelected?: boolean;
+
+    /**
+     * The index number of the list item when in a list.
+     */
     index: number;
+
+    /**
+     * The event called when the item is clicked.
+     */
     onClick: () => void;
+
+    /**
+     * The event called when the mouse rolls over the item.
+     */
     onMouseover: () => void;
+
+    /**
+     * The `id` attribute of the popover this option is associated with.
+     */
     popoverId: string;
+
+    /**
+     * Sets the underlying, hidden input element `value` attribute.
+     */
     value?: string;
   };
   Blocks: {
