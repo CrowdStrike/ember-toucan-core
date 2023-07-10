f26e798531f0dd696c040f664df6818d03d72d2b
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-07-10 08:21:29 -0400

    embroider: Update embroider deps+setup
    Co-authored-by: NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
-  let releaseVersion = await getChannelURL('release');
-
   return {
     usePnpm: true,
     scenarios: [
@@ -41,30 +38,6 @@ module.exports = async function () {
           },
         },
       },
-      embroiderOptimized({
-        name: 'ember-lts-4.8 + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-release + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': releaseVersion,
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
-          },
-        },
-      }),
     ],
   };
 };
