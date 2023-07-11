1111e1bd0cb0b325d8c00c248d70ab053b9c14f4
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2023-07-11 13:30:19 -0400

    Temporarily disable embroider-optimized tests
    https://github.com/CrowdStrike/ember-toucan-core/issues/210

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderOptimized } = require('@embroider/test-setup');
+// @todo Temporarily disabling embroider optimized tests due to https://github.com/CrowdStrike/ember-toucan-core/issues/210
+// const { embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
-  let releaseVersion = await getChannelURL('release');
+  // @todo Temporarily disabling embroider optimized tests due to https://github.com/CrowdStrike/ember-toucan-core/issues/210
+  // let releaseVersion = await getChannelURL('release');
 
   return {
     usePnpm: true,
@@ -41,30 +43,32 @@ module.exports = async function () {
           },
         },
       },
-      embroiderOptimized({
-        name: 'ember-lts-4.8 + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
-          },
-        },
-      }),
-      embroiderOptimized({
-        name: 'ember-release + embroider-optimized',
-        npm: {
-          devDependencies: {
-            'ember-source': releaseVersion,
-            // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
-            '@embroider/core': '2.1.2-unstable.c58f146',
-            '@embroider/compat': '2.1.2-unstable.c58f146',
-            '@embroider/webpack': '2.1.2-unstable.c58f146',
-          },
-        },
-      }),
+      // @todo Temporarily disabling embroider optimized tests due to https://github.com/CrowdStrike/ember-toucan-core/issues/210
+
+      // embroiderOptimized({
+      //   name: 'ember-lts-4.8 + embroider-optimized',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': '~4.8.0',
+      //       // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
+      //       '@embroider/core': '2.1.2-unstable.c58f146',
+      //       '@embroider/compat': '2.1.2-unstable.c58f146',
+      //       '@embroider/webpack': '2.1.2-unstable.c58f146',
+      //     },
+      //   },
+      // }),
+      // embroiderOptimized({
+      //   name: 'ember-release + embroider-optimized',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': releaseVersion,
+      //       // @todo remove this once we have a stable release that includes https://github.com/embroider-build/embroider/pull/1383, which should be https://github.com/embroider-build/embroider/pull/1408
+      //       '@embroider/core': '2.1.2-unstable.c58f146',
+      //       '@embroider/compat': '2.1.2-unstable.c58f146',
+      //       '@embroider/webpack': '2.1.2-unstable.c58f146',
+      //     },
+      //   },
+      // }),
     ],
   };
 };
